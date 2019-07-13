ffbe2a021109956d797c5f4a90a85d9e81b00c60
Gustavo Vitaliani <anabel_vitaliani@hotmail.com>
2019-07-13 18:07:53 -0300

    avance js

--- public/js/register.js
+++ public/js/register.js
@@ -2,6 +2,10 @@ window.onload = function () {
 
 var regexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
+var form = document.querySelector('form');
+var email = document.querySelector('#email');
+var div = document.querySelector('.div');
+
 function validarVacio(input) {
     if (input.value.trim() == '') {
         pintarError(input, 'Por favor, completá este campo');
@@ -15,76 +19,85 @@ function pintarError(input, mensaje) {
     var mark = document.createElement('mark');
     mark.innerText = mensaje;
     var div = elemento.parentElement;
-    if (div.children[2]) {
-        div.removeChild(div.children[2]);
-    }
+
     div.append(mark);
-  //  div.append(error);
 }
 
-  var form = document.querySelector('form');
 
-  // email.onblur = function () {
-  //     if (regexEmail.test(this.value)) {
-  //         this.classList.remove('is-invalid');
-  //     }
-  // }
 
-  form.onsubmit = function (event) {
-      var elementos = this.elements;
+form.onsubmit = function (event) {
+
+  if (div.children[2]) {
+        div.removeChild(div.children[2]);
+  }
+  var elementos = this.elements;
       //console.log(elementos);event.preventDefault();
-      for (elemento of elementos) {
-          if (elemento.type == 'submit' || elemento.type == 'hidden' || elemento.type == 'reset' ) {
-              continue;
-          }
-          if (elemento.type != 'file' && validarVacio(elemento)) {
-              event.preventDefault();
-          } else if (elemento.name == 'email' && !elemento.parentElement.children[2] && !regexEmail.test(elemento.value)) {
-              pintarError(elemento, 'Email invalido');
-              event.preventDefault();
-          }
+  for (elemento of elementos) {
 
-          if (elemento.name == 'password') {
-              var confirm = document.querySelector('#password_confirmation');
-              if (elemento.value.length < 8) {
-                  pintarError(elemento, 'El password debe tener minimo 8 caracteres');
-                  event.preventDefault();
-              } else if (elemento.value != confirm.value) {
-                  pintarError(elemento, 'Las contraseñas no coinciden');
-                  event.preventDefault();
-              }
-          }
-      }
+  if (elemento.type == 'submit' || elemento.type == 'hidden' || elemento.type == 'reset' || elemento.id == 'comentarios') {
+    continue;
   }
 
-var elementos = form.elements;
-
-for (elemento of elementos) {
-
-  elemento.onblur = function() {
-    if (this.type != 'submit' && this.type != 'hidden' && this.type != 'reset' ) {
-
-      //console.log(!validarVacio(this), this.parentElement)
-      if (!validarVacio(this)) {
-        console.log(elemento.name, !regexEmail.test(elemento.value))
-        if (this.parentElement.children[2]) {
-          this.parentElement.removeChild(this.parentElement.children[2]);
-          if (elemento.name == 'email' &&  !regexEmail.test(elemento.value)) {
-            pintarError(elemento, 'Email invalido');
+  if (elemento.type != 'file' && validarVacio(elemento)) {
+    event.preventDefault();
+  } else if (elemento.name == 'email' && !regexEmail.test(elemento.value)) {
+        pintarError(elemento, 'Email invalido');
+        event.preventDefault();
+  } else if (elemento.name == 'password') {
+        var confirm = document.querySelector('#password_confirmation');
+        if (elemento.value.length < 8) {
+            pintarError(elemento, 'El password debe tener minimo 8 caracteres');
+            event.preventDefault();
+        } else if (elemento.value != confirm.value) {
+              pintarError(elemento, 'Las contraseñas no coinciden');
+              event.preventDefault();
           }
-        }
-      }
-
-      if (elemento.name == 'email' &&  !regexEmail.test(elemento.value)) {
-        console.log('bla');
-        if (this.parentElement.children[2]) {
-          this.parentElement.removeChild(this.parentElement.children[2]);
-            }
-          }
-
-      }
     }
   }
+}
+
+email.onblur = function () {
+  console.log(email);
+  if (div.children[2]) {
+        div.removeChild(div.children[2]);
+  }
+  if (validarVacio(this)) {
+    validarVacio(this);
+  } else if(!regexEmail.test(elemento.value))
+      pintarError(elemento, 'Email invalido');
+}
+
+/*var elementos = this.elements;
+
+for (elemento of elementos) {
+elemento.onblur = function() {
+
+      if (div.children[2]) {
+            div.removeChild(div.children[2]);
+      }
+
+          //console.log(elementos);event.preventDefault();
 
 
+      if (elemento.type == 'submit' || elemento.type == 'hidden' || elemento.type == 'reset' || elemento.id == 'comentarios') {
+        continue;
+      }
+
+      if (elemento.type != 'file' && validarVacio(elemento)) {
+        event.preventDefault();
+      } else if (elemento.name == 'email' && !regexEmail.test(elemento.value)) {
+            pintarError(elemento, 'Email invalido');
+            event.preventDefault();
+      } else if (elemento.name == 'password') {
+            var confirm = document.querySelector('#password_confirmation');
+            if (elemento.value.length < 8) {
+                pintarError(elemento, 'El password debe tener minimo 8 caracteres');
+                event.preventDefault();
+            } else if (elemento.value != confirm.value) {
+                      pintarError(elemento, 'Las contraseñas no coinciden');
+                      event.preventDefault();
+              }
+        }
+      }
+}*/
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,54 +22,54 @@
         @endif
         <h1 style="color:white">Registrate</h1>
 
-    <div class="">
+    <div class="div">
       <label for="name" class="label-registro">Nombre</label>
       <input id="name" type="text" name="name" value="{{old('name')}}" placeholder="Ingrese su nombre *">
     </div>
 
-    <div>
+    <div class = "div">
     <label for="surname" class="label-registro">Apellido</label>
     <input id="surname"type="text" name="surname" value="{{old('surname')}}" placeholder="Ingrese su apellido *">
     </div>
 
-    <div>
+    <div class = "div">
     <label for="email" class="label-registro">Email</label>
     <input id="email" type="email" name="email" value="{{old('email')}}"placeholder="nombre@nombre.com *">
     </div>
 
-    <div>
+    <div class = "div">
     <label for="fecha" class="label-registro">Fecha de nacimiento</label>
     <input id="fecha"type="date" name="fecha" value="{{old('fecha')}}">
     </div>
 
-    <div>
+    <div class = "div">
     <label for="password" class="label-registro">Contraseña</label>
     <input id="password" type="password" name="password" value="" placeholder="Contraseña *">
 
   </div>
-    <div>
+    <div class = "div">
     <label for="password_confirmation" class="label-registro">Confirmar contraseña</label>
     <input id="password_confirmation" type="password" name="password_confirmation" value="" placeholder="Confirme contraseña *">
 
   </div>
-    <div>
+    <div class = "div">
       <label for="avatar" class="label-registro">Foto de perfil</label>
       <input type="file" name="avatar" value="">
     </div>
 
-    <div>
+    <div class = "div">
     <label for="telefono" class="label-registro">Teléfono</label>
     <input id="telefono" type="text" name="telefono" value="{{old('telefono')}}" placeholder="Número de teléfono *">
   </div>
 
-    <div>
+    <div class = "div">
     <br>
     <label class="com"for="com" class="label-registro" id="comentarios">COMENTARIOS</label>
     <br>
     <textarea class="coment" id="com" name="com" rows="8" cols="80"></textarea>
   </div>
 
-    <div>
+    <div class="div">
     <button class="boton"type="submit" name="button" value="Enviar Datos">ENVIAR</button>
     <button class="resetear"type="reset" name="button">REINICIAR</button>
   </div>
@@ -80,5 +80,6 @@
     </main>
 
   </div>
-<script type="text/javascript" src="/js/register.js"></script>
+
+<script type class = "div"="text/javascript" src="/js/register.js"></script>
 @endsection
