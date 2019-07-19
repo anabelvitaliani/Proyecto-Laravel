2de6490e64ff86a6a0d0214b39c427dbb4dea72c
Gustavo Vitaliani <anabel_vitaliani@hotmail.com>
2019-07-18 23:17:08 -0300

    padding

--- public/js/auth/register.js
+++ public/js/auth/register.js
@@ -76,7 +76,7 @@ form.onsubmit = function (event) {
       event.preventDefault();
     }
     if (elemento.name == 'email' && regexEmail.test(elemento.value)) {
-      fetch('http://chocolatemia.dhalumnos.com/api/buscarEmail?email='+ elemento.value)
+      fetch('http://www.chocolatemia.dhalumnos.com/api/buscarEmail?email='+ elemento.value)
       .then( (response) => { return response.json(); })
       .then( (datos) => {
         if (datos.existe) {
